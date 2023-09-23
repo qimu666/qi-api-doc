df882ff4de023a594bdad5cc69286ac96a24cc27
qimu <2483482026@qq.com>
2023-09-23 21:06:43 +0800

    fix: 补充接口文档

--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -5,6 +5,7 @@ module.exports = [
             {text: '简 介', link: '/pages/a2f161/'},
             {text: '安 装', link: '/pages/2f674a/'},
             {text: '快速开始', link: '/pages/793dcb/'},
+            {text: '返回响应码', link: '/pages/745dcb/'},
             {
                 text: 'API接口', items: [
                     {text: '随机毒鸡汤', link: '/pages/1705234447153963010/'},
@@ -14,7 +15,7 @@ module.exports = [
                     {text: '获取天气信息', link: '/pages/1705240565347459073/'},
                     {text: '随机壁纸', link: '/pages/1705237990061580289/'},
                 ]
-            }
+            },
         ]
     },
     {
@@ -22,7 +23,7 @@ module.exports = [
             {text: '使用配置', link: '/pages/a20ce8/'},
         ]
     },
-    {text: '问答', link: '/pages/9cc27d/'},
+    // {text: '问答', link: '/pages/9cc27d/'},
     {text: '赞助', link: '/pages/1b12ed/'},
     {text: 'Demo', link: 'https://github.com/qimu666/qi-api-sdk-demo/blob/master/src/main/java/icu/qimuu/qiapisdkdemo/controller/InvokeController.java'},
 ]
